ba3eb7a7773b4d87d10e1f4631662a749c5d9f1a
misael soares <misasoaresms2@gmail.com>
2023-10-25 21:56:36 -0300

    add username e name no retweet

--- src/service/tweet.service.ts
+++ src/service/tweet.service.ts
@@ -3,64 +3,71 @@ import { ResponseDto } from "../dtos/response.dto";
 import { CreateTweet, DeleteTweetDto, UpdateTweetDto } from "../dtos/tweet.dto";
 import { Tweet } from "../model/tweet.model";
 
-
 class TweetService {
-  public async list(userID:string): Promise<ResponseDto> {
+  public async list(userID: string): Promise<ResponseDto> {
     const result = await repository.tweet.findMany({
-      include:{
-        User:{
-          select:{
-            id:true,
-            username:true,
-            name:true,
-            LikesToUser:{
-              select:{
-                id:true,
-                retweetId:true,
-                tweetId:true,
-                userId:true
-              },where:{
-                userId:userID
-              }
+      include: {
+        User: {
+          select: {
+            id: true,
+            username: true,
+            name: true,
+            LikesToUser: {
+              select: {
+                id: true,
+                retweetId: true,
+                tweetId: true,
+                userId: true,
+              },
+              where: {
+                userId: userID,
+              },
             },
-          }
-        },Likes:{
-          select:{
-            id:true,
-            userId:true,
-            tweetId:true,
-            retweetId:true
-          }
-        },Retweet:{
-          select:{
-            id:true,
-            content:true,
-            Likes:{
-              select:{
-                id:true,
-                userId:true,
-                TweetId:{
-                  select:{
-                    content:true,
-                    id:true,
-                    Likes:{
-                      select:{
-                        id:true
-                      }
-                    }
-                  }
-                }
-              }
-            }
-            
-          }
-        }
-        
-      },orderBy:[
+          },
+        },
+        Likes: {
+          select: {
+            id: true,
+            userId: true,
+            tweetId: true,
+            retweetId: true,
+          },
+        },
+        Retweet: {
+          select: {
+            id: true,
+            content: true,
+            User: {
+              select: {
+                name: true,
+                username: true,
+              },
+            },
+            Likes: {
+              select: {
+                id: true,
+                userId: true,
+                TweetId: {
+                  select: {
+                    content: true,
+                    id: true,
+                    Likes: {
+                      select: {
+                        id: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      orderBy: [
         {
-          createdAt:'desc'
-        }
-      ]
+          createdAt: "desc",
+        },
+      ],
     });
     return {
       code: 200,
@@ -68,28 +75,27 @@ class TweetService {
       data: result,
     };
   }
-  public async listByIdUser(userID: string): Promise<ResponseDto>  {
+  public async listByIdUser(userID: string): Promise<ResponseDto> {
     const data = await repository.tweet.findMany({
       where: {
         userId: userID,
       },
-      include:{
-        User:{
-          select:{
-            username:true,
-          }
+      include: {
+        User: {
+          select: {
+            username: true,
+          },
         },
-        
-      }
+      },
     });
     return {
-      code:200,
-      message:`Lista de todos os tweets do usuário: ${data[0].User.username}`,
-      data:data
+      code: 200,
+      message: `Lista de todos os tweets do usuário: ${data[0].User.username}`,
+      data: data,
     };
   }
 
-  public async create(data: CreateTweet): Promise<ResponseDto>  {
+  public async create(data: CreateTweet): Promise<ResponseDto> {
     const tweet = new Tweet(data.content, data.type, data.userID);
 
     const createdTweet = await repository.tweet.create({
@@ -100,31 +106,33 @@ class TweetService {
       },
     });
     return {
-      code:201,
-      message:`Você tweetou: '${createdTweet.content}'`,
-      data:createdTweet
+      code: 201,
+      message: `Você tweetou: '${createdTweet.content}'`,
+      data: createdTweet,
     };
   }
 
-  public async showUniqueTweet(id: string, userID:string): Promise<ResponseDto> {
+  public async showUniqueTweet(id: string, userID: string): Promise<ResponseDto> {
     const result = await repository.tweet.findUnique({
       where: {
         id,
-      },include:{
-        Likes:{
-          select:{
-            userId:true
-          },where:{
-            userId:userID
-          }
-        }
-      }
+      },
+      include: {
+        Likes: {
+          select: {
+            userId: true,
+          },
+          where: {
+            userId: userID,
+          },
+        },
+      },
     });
 
     return {
-      code:200,
-      message:`Mostrar apenas um tweet:`,
-      data:result
+      code: 200,
+      message: `Mostrar apenas um tweet:`,
+      data: result,
     };
   }
 
@@ -136,31 +144,31 @@ class TweetService {
       },
       data: {
         content: data.content,
-      },include:{
-        User:{
-          select:{
-            username:true
-          }
-        }
-      }
+      },
+      include: {
+        User: {
+          select: {
+            username: true,
+          },
+        },
+      },
     });
     return {
-      code:200,
-      message:`Tweet atualizado com sucesso.`,
-      data:result
+      code: 200,
+      message: `Tweet atualizado com sucesso.`,
+      data: result,
     };
   }
 
-  public async deleteAll():Promise<ResponseDto> {
+  public async deleteAll(): Promise<ResponseDto> {
     const result = await repository.tweet.deleteMany();
     return {
-      code:200,
-      message:`Tweets deletados com sucesso.`,
-      data:result
+      code: 200,
+      message: `Tweets deletados com sucesso.`,
+      data: result,
     };
   }
-  public async delete(data: DeleteTweetDto):Promise<ResponseDto> {
-
+  public async delete(data: DeleteTweetDto): Promise<ResponseDto> {
     const result = await repository.tweet.delete({
       where: {
         userId: data.userID,
@@ -168,9 +176,9 @@ class TweetService {
       },
     });
     return {
-      code:200,
-      message:`Tweet deletado com sucesso.`,
-      data:result
+      code: 200,
+      message: `Tweet deletado com sucesso.`,
+      data: result,
     };
   }
 }
