d841cd7064a93874ef2adebe8005836cfa0f8946
misael soares <misasoaresms2@gmail.com>
2023-10-28 23:41:18 -0300

    icones funcionando

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,37 +1,35 @@
-import { NextFunction, Request, Response } from 'express';
-import userService from '../service/user.service';
-
+import { NextFunction, Request, Response } from "express";
+import userService from "../service/user.service";
 
 async function authMiddleware(req: Request, res: Response, next: NextFunction) {
-	try {
-		const token = req.headers.authorization;
+  try {
+    const token = req.headers.authorization;
 
-		if (!token) {
-			return res.status(401).send({
-				code: 401,
-				message: 'Authentication token fail',
-			});
-		}
-	
-		const result = await userService.getUserByToken(token as string);
-		
-		if (!result.data) {
-			return res.status(401).send({
-				code: 401,
-				message: 'Authentication token fail',
-			});
-		}
-		
-		
-		req.body.userID = result.data.id;
-		
-		next();
-	} catch (error) {
-		return res.status(500).send({
-			code: 500,
-			message: error,
-		});
-	}
+    if (!token) {
+      return res.status(401).send({
+        code: 401,
+        message: "Authentication token fail",
+      });
+    }
+
+    const result = await userService.getUserByToken(token as string);
+
+    if (!result.data) {
+      return res.status(401).send({
+        code: 401,
+        message: "Authentication token fail",
+      });
+    }
+
+    req.body.userID = result.data.id;
+
+    next();
+  } catch (error) {
+    return res.status(500).send({
+      code: 500,
+      message: error,
+    });
+  }
 }
 
 export default authMiddleware;
